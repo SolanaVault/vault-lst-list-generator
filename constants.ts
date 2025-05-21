cb1142acdac55b3b255189b530e7391434f0efde
x <x>
2025-05-21 13:17:07 +0200

    Add in LST lists

---
+++ constants.ts
@@ -0,0 +1,9 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const STAKE_POOL_PROGRAM_ID = new PublicKey(
+  "SPoo1Ku8WFXoNDMHPsrGSTSG1Y47rzgn41SLUNakuHy"
+);
+
+export const SANCTUM_PROGRAM_ID = new PublicKey(
+  "SP12tWFxD9oJsVWNavTTBZvMbA6gkAmxtVgxdqvyvhY"
+);
